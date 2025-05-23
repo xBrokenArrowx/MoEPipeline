66515771515a6d5dfaddf0d3f5abcfd0f7262015
wcblanco <wcblanco@nlp-gpu-02.be.ucsc.edu>
2025-05-23 15:48:42 -0700

    changed result and score storage

--- run_bfcl.sh
+++ run_bfcl.sh
@@ -3,11 +3,15 @@ bfcl generate \
     --model $1 \
     --test-category single_turn \
     --backend vllm \
-    --local-model-path $2
+    --local-model-path $2 \
+    --result-dir ../results/
+
 
 bfcl evaluate \
     --model $1 \
-    --test-category single_turn 
+    --test-category single_turn \
+    --result-dir ../results/ \
+    --score-dir ../scores/
 
 
 
