ef6eaf97ee790f19e7c8c6a1fa9ff464756a629e
sarumsey <sarumsey@nlp-gpu-02.be.ucsc.edu>
2025-06-10 11:20:34 -0700

    testing on 2 epochs for lora

--- run_moe.sh
+++ run_moe.sh
@@ -7,14 +7,14 @@
 #
 # example of calling this script: ./run_moe.sh Qwen/Qwen2.5-1.5B-Instruct yahma/alpaca-cleaned my_cool_adapter
 batch=64
-epochs=1
-export CUDA_VISIBLE_DEVICES=1
+epochs=2
+# export CUDA_VISIBLE_DEVICES=1
 
 echo "Generating Config"
 echo "Making new model in models/$3"
 
 python ./MoE-PEFT/launch.py gen \
-    --template mixlora \
+    --template lora \
     --tasks $2 \
     --adapter_name "models/$3" \
     --batch_size $batch \
